3c1ba6494b1ee5ae4a7fec8c3b8ecf493a9fea6a
kkempski <kkempski@nmg.pl>
2025-07-27 14:10:56 +0200

    Dodanie konwerterów, które umożliwiają wyświetlanie informacji o pogodzie i odpowiedniej animacji prezentującej pogodę.

---
+++ Converters/CodeToLottieConverter.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json.Linq;
+using SkiaSharp.Extended.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiWeather.Converters
+{
+    public class CodeToLottieConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            float code = System.Convert.ToSingle(value);
+            SKFileLottieImageSource lottieImageSource = new SKFileLottieImageSource();
+
+            switch (code)
+            {
+                case 0:
+                    lottieImageSource.File = "sunny.json";
+                    return lottieImageSource;
+                case 1:
+                case 2:
+                case 3:
+                    lottieImageSource.File = "partly-cloudy.json";
+                    return lottieImageSource;
+                case 45:
+                case 48:
+                    lottieImageSource.File = "mist.json";
+                    return lottieImageSource;
+                case 51:
+                case 53:
+                case 55:
+                    lottieImageSource.File = "rain.json";
+                    return lottieImageSource;
+                case 56:
+                case 57:
+                    lottieImageSource.File = "rain.json";
+                    return lottieImageSource;
+                case 61:
+                case 63:
+                case 65:
+                    lottieImageSource.File = "rain.json";
+                    return lottieImageSource;
+                case 66:
+                case 67:
+                    lottieImageSource.File = "rain.json";
+                    return lottieImageSource;
+                case 71:
+                case 73:
+                case 75:
+                    lottieImageSource.File = "snow.json";
+                    return lottieImageSource;
+                case 77:
+                    lottieImageSource.File = "snow.json";
+                    return lottieImageSource;
+                case 80:
+                case 81:
+                case 82:
+                    lottieImageSource.File = "rain.json";
+                    return lottieImageSource;
+                case 85:
+                case 86:
+                    lottieImageSource.File = "snow.json";
+                    return lottieImageSource;
+                case 95:
+                    lottieImageSource.File = "thunder.json";
+                    return lottieImageSource;
+                case 96:
+                case 99:
+                    lottieImageSource.File = "thunder.json";
+                    return lottieImageSource;
+                default:
+                    lottieImageSource.File = "sunny.json";
+                    return lottieImageSource;
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/CodeToWeatherConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiWeather.Converters
+{
+    public class CodeToWeatherConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            float code = System.Convert.ToSingle(value);
+
+            switch (code)
+            {
+                case 0:
+                    return "Pogodnie";
+                case 1:
+                case 2:
+                case 3:
+                    return "Częściowe zachmurzenie";
+                case 45:
+                case 48:
+                    return "Mgła";
+                case 51:
+                case 53:
+                case 55:
+                    return "Mżawka";
+                case 56:
+                case 57:
+                    return "Marznąca mżawka";
+                case 61:
+                case 63:
+                case 65:
+                    return "Deszcz";
+                case 66:
+                case 67:
+                    return "Marznący deszcz";
+                case 71:
+                case 73:
+                case 75:
+                    return "Śnieg";
+                case 77:
+                    return "Śnieg ziarnisty";
+                case 80:
+                case 81:
+                case 82:
+                    return "Przelotny deszcz";
+                case 85:
+                case 86:
+                    return "Przelotny śnieg";
+                case 95:
+                    return "Burze";
+                case 96:
+                case 99:
+                    return "Burze z gradem";
+                default:
+                    return "Nieznana pogoda";
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
