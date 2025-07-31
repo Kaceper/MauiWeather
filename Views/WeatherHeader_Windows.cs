602ac95ffd2eabafe285ef2bcc616a2dcfe7edaa
kkempski <kkempski@nmg.pl>
2025-07-31 23:59:59 +0200

    Dodanie wyświetlania ostrzeżeń meteorologicznych.

---
+++ Views/WeatherHeader_Windows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiWeather.Views
+{
+    public partial class WeatherHeader_Windows : ContentView
+    {
+        public WeatherHeader_Windows()
+        {
+            InitializeComponent();
+        }
+    }
+}
